2bcd95a26ee87541f9791cca6a12cb4acc33fd1e
mohamed-dhia-ben-amar <mohameddhia.benamar@esprit.tn>
2022-10-10 12:49:34 +0100

    S4

--- S4/Controllers/Users.controller.js
+++ S4/Controllers/Users.controller.js
@@ -3,37 +3,32 @@ import User from '../Models/Users.model.js';
 
 
 
-export function update(req, res) {
+export async function update(req, res) {
     /*var user = Users.find((u => (u.id === req.body.id)))
     user.username = req.body.username
     user.password = req.body.password
     user.wallet = req.body.wallet
     res.status(200).json({username : user.username, wallet : user.wallet}) */
-    User
+    var user = await User
         .findOneAndUpdate({ "name": req.params.username }, { "password": req.body.password, "wallet": req.body.wallet })
-        .then(docs => {
-            res.status(200).json(docs);
-        })
-        .catch(err => {
-            res.status(200).json({ error: err })
-        })
+    res.status(200).json({ username: user.username, wallet: user.wallet });
 }
 
 export function LogIn(req, res) {
     /* var user = Users.find(u => (u.username === req.body.username) && (u.password === req.body.password))
     res.status(200).json({ username: user.username, wallet: user.wallet }) */
     User
-    .find({})
-    .where('username').equals(req.body.username)
-    .where('password').equals(req.body.password)
-    .limit(1)
-    .exec
-    .then(docs => {
-        res.status(200).json(docs);
-    })
-    .catch(err => {
-        res.status(200).json({ error: err })
-    })
+        .find({})
+        .where('username').equals(req.body.username)
+        .where('password').equals(req.body.password)
+        .limit(1)
+        .exec()
+        .then(docs => {
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            res.status(200).json({ error: err })
+        })
 }
 
 export function register(req, res) {
--- S4/server.js
+++ S4/server.js
@@ -1,25 +1,44 @@
 import express from 'express'; //ekma js
-import mongoose, { mongo } from 'mongoose';
-
-import UserRouter from './Routes/Users.route.js';
+import mongoose from 'mongoose';
+import { notFoundError, errorHandler } from './Middleware/error-handler.js'
+import morgan from 'morgan';
 import GameRouter from './Routes/Games.route.js';
-import AchatRouter from './Routes/Achats.route.js';
-
 
 const app = express();
 const database = "workshop4SIM5"
-const hostname = '127.0.0.1';
 const port = process.env.port || 3000;
-
-app.use(express.json())
-app.use('/user',UserRouter)
-app.use('/game',GameRouter)
-app.use('/achat',AchatRouter)
+const hostname = '127.0.0.1';
 
 mongoose.set('debug', true)
 mongoose.Promise = global.Promise
 
-mongoose.connect(`mongodb://localhost:27017/${database}`)
+mongoose
+    .connect(`mongodb://localhost:27017/${database}`)
+    .then(() => {
+        console.log(`connected to  ${database}`)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+
+app.use(morgan("dev"))
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log("Middleware just ran")
+    next()
+})
+
+app.use("/gse", (req, res, next) => {
+    console.log("Middleware just ran on gse route")
+    next()
+})
+
+app.use('/game', GameRouter)
+
+app.use(errorHandler)
+app.use(notFoundError)
 
 app.listen(port, hostname, () => {
     console.log(`Server running at https://${hostname}:${port}/`);
